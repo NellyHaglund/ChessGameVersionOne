8621067d93eceed2056f6a552edc30bee9a5180d
Nelly <haglundnelly@gmail.com>
2014-10-30 13:21:07 +0100

    k

--- Schack/ChessGame/StartClass.cs
+++ Schack/ChessGame/StartClass.cs
@@ -10,59 +10,41 @@ namespace ChessGame
     {
         public void Start()
         {
+         
+            GameBoard gameBoard = new GameBoard();
+            //vita bönder
+          //  List<Piece> WhitePawn = new List<Piece>();
+
+
+            gameBoard.board[1, 0] = new Pawn(new Position(1, 0), colour.white);
+            gameBoard.board[1, 1] = new Pawn(new Position(1, 1), colour.white);
+            gameBoard.board[1, 2] = new Pawn(new Position(1, 2), colour.white);
+            gameBoard.board[1, 3] = new Pawn(new Position(1, 3), colour.white);
+            gameBoard.board[1, 4] = new Pawn(new Position(1, 4), colour.white);
+            gameBoard.board[1, 5] = new Pawn(new Position(1, 5), colour.white);
+            gameBoard.board[1, 6] = new Pawn(new Position(1, 6), colour.white);
+            gameBoard.board[1, 7] = new Pawn(new Position(1, 7), colour.white);
+
+
+            // svarta bönder
+
+            gameBoard.board[6, 0] = new Pawn(new Position(6, 0), colour.black);
+            gameBoard.board[6, 1] = new Pawn(new Position(6, 1), colour.black);
+            gameBoard.board[6, 2] = new Pawn(new Position(6, 2), colour.black);
+            gameBoard.board[6, 3] = new Pawn(new Position(6, 3), colour.black);
+            gameBoard.board[6, 4] = new Pawn(new Position(6, 4), colour.black);
+            gameBoard.board[6, 5] = new Pawn(new Position(6, 5), colour.black);
+            gameBoard.board[6, 6] = new Pawn(new Position(6, 6), colour.black);
+            gameBoard.board[6, 7] = new Pawn(new Position(6, 7), colour.black);
+                                                              
+
+
             Console.WriteLine("Start the game? y/n");
             string answer = Console.ReadLine();
 
-            Piece piece = new Piece();
-            piece.whitePawn = 15;
-            piece.blackPawn = 05;
+        
 
-            
-
-            var blackPawn1 = piece.blackPawn;
-            var blackPawn2 = piece.blackPawn;
-            var blackPawn3 = piece.blackPawn;
-            var blackPawn4 = piece.blackPawn;
-            var blackPawn5 = piece.blackPawn;
-            var blackPawn6 = piece.blackPawn;
-            var blackPawn7 = piece.blackPawn;
-            var blackPawn8 = piece.blackPawn;
-
-            var whitePawn1 = piece.whitePawn;
-            var whitePawn2 = piece.whitePawn;
-            var whitePawn3 = piece.whitePawn;
-            var whitePawn4 = piece.whitePawn;
-            var whitePawn5 = piece.whitePawn;
-            var whitePawn6 = piece.whitePawn;
-            var whitePawn7 = piece.whitePawn;
-            var whitePawn8 = piece.whitePawn;
-
-
-
-            GameBoard gameBoard = new GameBoard();
            
-
-
-            gameBoard.board[1, 0] = whitePawn1;
-            gameBoard.board[1, 1] = whitePawn2;
-            gameBoard.board[1, 2] = whitePawn3;
-            gameBoard.board[1, 3] = whitePawn4;
-            gameBoard.board[1, 4] = whitePawn5;
-            gameBoard.board[1, 5] = whitePawn6;
-            gameBoard.board[1, 6] = whitePawn7;
-            gameBoard.board[1, 7] = whitePawn8;
-
-            gameBoard.board[6, 0] = blackPawn1;
-            gameBoard.board[6, 1] = blackPawn2;
-            gameBoard.board[6, 2] = blackPawn3;
-            gameBoard.board[6, 3] = blackPawn4;
-            gameBoard.board[6, 4] = blackPawn5;
-            gameBoard.board[6, 5] = blackPawn6;
-            gameBoard.board[6, 6] = blackPawn7;
-            gameBoard.board[6, 7] = blackPawn1;
-
-
-            
 
 
             if (answer == "y" || answer == "Y")
