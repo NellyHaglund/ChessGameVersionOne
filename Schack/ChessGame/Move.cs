41b3d365a74ebb53735de74f79b5416e0c8ed505
Jakob <jakob.stjerna@gmail.com>
2014-10-31 09:04:27 +0100

    Lagt till fler kommentarer plus lekt lite med Random klassen
    Har lagt till lite kommentarer i vad vi kan göra med vissa metoder, samt
    lagt till en Random klass

--- Schack/ChessGame/Move.cs
+++ Schack/ChessGame/Move.cs
@@ -8,12 +8,34 @@ namespace ChessGame
 {
     class Move
     {
+        GameBoard gameBoard = new GameBoard();
 
         public void MovePawn()
         {
-            GameBoard gameBoard = new GameBoard();
+            
 
             gameBoard.board[4, 3] = new Pawn(new Position(4, 3), colour.black);
         }
+        Random random = new Random();
+
+        public void RandomizeMove()
+        {
+            int x = random.Next((8));
+            int y = random.Next((8));
+            gameBoard.board[x, y] = null;
+        }
+       
+
+
+
+
+
+
+
+
+        //random on which pawn the player wants to move
+        //then go in the loop, which will have a lot of
+        //if/else. then g outside the loop when the piece
+        //have moved
     }
 }
--- Schack/ChessGame/Pawn.cs
+++ Schack/ChessGame/Pawn.cs
@@ -18,9 +18,11 @@ namespace ChessGame
 
         public void MovePawn(Position pps)
         {
-         //ToDo. this method will decide how the 
-         // Pawn can move in the Game
-
+           //ToDo. this method will decide how the 
+           // pawn can move in the game
+           // Try use bool moved, if moved = false        
+           // it can move two or one step
+           // if moved = true, it can only move one step
         }
                                                
         public override string ToString()       // Method that Prints position of Pawn
